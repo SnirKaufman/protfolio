cd7185f9412bda7e44cce57adb792835747c0cbb
snir <snirkaufman4@gmail.com>
2023-07-25 15:05:30 +0300

    almost finished hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,28 +1,35 @@
 import "./Hero.css";
 import waveHand from "../../assets/wave.png";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { GrLocationPin } from "react-icons/gr";
+import snirPic from "../../assets/Snir.jpg";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-text-container">
-        <div className="hero-title">
-          <h1>Front-End React Developer</h1>
-          <img src={waveHand} alt="waving_hand" />
+    <>
+      <div className="hero">
+        <div className="hero-text-container">
+          <h1>
+            Front-End React Developer
+            <img src={waveHand} alt="waving_hand" className="waving-hand" />
+          </h1>
+          <p>
+            Hi, I'm Snir Kaufman. A passionate Front-end React Developer based
+            in Israel. 📍
+          </p>
+          <div className="hero-icons-container">
+            <BsLinkedin size={30} className="icons" />
+            <BsGithub size={30} className="icons" />
+          </div>
         </div>
-        <p>
-          Hi, I'm Snir Kaufman. A passionate Front-end React Developer based in
-          Israel.
-          <GrLocationPin className="icons location" />
-        </p>
-        <div className="hero-icons-container">
-          <BsGithub size={40} className="icons" />
-          <BsLinkedin size={40} className="icons" />
+        <div className="hero-img">
+          <img src={snirPic} alt="snir" />
         </div>
       </div>
-      <div className="hero-img"></div>
-    </div>
+      <div className="tech-stack-container">
+        <div className="tech-stack">Tech Stack</div>
+        <div className="tech-stack-icons"></div>
+      </div>
+    </>
   );
 };
 
